cd76134a21e76bd6236cfc8baa1fae0a48aa69b8
xmacur06 <macura.jm@seznam.cz>
2017-03-31 21:56:46 +0200

    Added TOP entity

---
+++ SOURCES/TOP.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:55:48 03/31/2017 
+-- Design Name: 
+-- Module Name:    TOP - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TOP is
+    Port ( clk : in  STD_LOGIC);
+end TOP;
+
+architecture Behavioral of TOP is
+
+begin
+
+
+end Behavioral;
+
